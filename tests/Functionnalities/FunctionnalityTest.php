4922a3ee557f09818102b487e3de9604e76cb2b8
thewrath <episajout@gmail.com>
2019-09-17 21:18:15 +0200

    Add phpunit tests

---
+++ tests/Functionnalities/FunctionnalityTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class FunctionnalityTest extends TestCase
+{
+    // public function testCanBeCreatedFromValidEmailAddress(): void
+    // {
+    
+    // }
+
+}
---
+++ tests/Functionnalities/HelloTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class HelloTest extends TestCase
+{
+    // public function testCanBeCreatedFromValidEmailAddress(): void
+    // {
+    
+    // }
+
+}
